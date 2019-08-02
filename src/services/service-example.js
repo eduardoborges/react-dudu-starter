bd8db1704d7132045f04de0ccfb5365fb957e85f
Eduardo Borges <euduardoborges@gmail.com>
2019-08-02 17:03:54 -0300

    Melhorias

---
+++ src/services/service-example.js
@@ -0,0 +1,24 @@
+
+class MyServiceCrud {
+  static index = () => {
+    //
+  };
+
+  static create = () => {
+    //
+  };
+
+  static read = () => {
+    //
+  };
+
+  static update = () => {
+    //
+  };
+
+  static delete = () => {
+    //
+  };
+}
+
+export default MyServiceCrud;
