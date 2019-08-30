092143455d9c6fcecf1ba971229a4e3412e3b148
Eduardo Borges <euduardoborges@gmail.com>
2019-08-30 11:43:14 -0300

    (chore): todo interface

--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -1,30 +1,53 @@
-/* eslint-disable react/destructuring-assignment */
-import * as React from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { connect } from 'unistore/react';
-import { TodoState } from 'store/todo/types';
+import { TodoState, TodoActions } from 'store/todo/types';
 import todoActions from 'store/todo/actions';
 
 interface StateProps {
   TODOS: TodoState
 }
-
-interface ActionsProps {
-  addTodo(): void
-}
-
 interface OwnProps {
   bar?:boolean
 }
 
-type Props = StateProps & ActionsProps & OwnProps & RouteComponentProps;
+type Props = StateProps & TodoActions & OwnProps & RouteComponentProps;
 
-const Login = (props:Props) => {
-  const { TODOS, addTodo } = props;
+const Login : React.FC<Props> = (props) => {
+  const {
+ TODOS, addTodo, removeTodo, toggleTodo,
+} = props;
+  const [value, setValue] = useState<string | undefined>('');
   return (
-    <div>
-      <button className="button" type="button" onClick={addTodo}>Add Tosdo</button>
-      <ul>{TODOS.data.map(todo => (<li>{todo.name}</li>))}</ul>
+    <div className="hero is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Hello world!</h1>
+          <div className="columns">
+            <div className="column">
+              <input className="input" value={value} onChange={e => setValue(e.target.value)} />
+            </div>
+            <div className="column">
+              <button className="button is-primary" type="button" onClick={() => addTodo(value)}>Add Todo</button>
+            </div>
+          </div>
+          <ul>
+            {TODOS.data.map(todo => (
+              <li>
+                <input checked={todo.done} type="checkbox" name="" id="" onChange={() => toggleTodo(todo)} />
+                {' '}
+                {todo.name}
+                {' '}
+                <button type="button" className="button is-small is-text" onClick={() => removeTodo(todo)}>remove</button>
+                {' '}
+              </li>
+            ))}
+
+          </ul>
+        </div>
+
+      </div>
+
     </div>
 );
 };
