171d78d21e0e1aebffa185d7e1b9d4892932f50e
Eduardo Borges <euduardoborges@gmail.com>
2019-08-30 11:43:24 -0300

    (chore): todo actions

--- src/store/todo/actions.ts
+++ src/store/todo/actions.ts
@@ -1,26 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { Store } from 'unistore';
-
 import { AppState } from '../index';
+import { Todo } from './types';
 
 export default (store:Store<AppState>) => ({
-    addTodo: (state: AppState) => ({
+    addTodo: (state: AppState, todoValue: string) => ({
       TODOS: {
         ...state.TODOS,
-        data: [...state.TODOS.data, { name: 'Teste', id: state.TODOS.data.length + 1, done: false }],
+        data: [...state.TODOS.data,
+          { name: todoValue, id: state.TODOS.data.length + 1, done: false },
+        ],
       },
     }),
 
-    toggleTodo: (state: AppState) => ({
+    toggleTodo: (state: AppState, todo: Todo) => ({
       TODOS: {
         ...state.TODOS,
+        data: state.TODOS.data.map(t => (t.id === todo.id ? ({ ...t, done: !t.done }) : t)),
       },
     }),
 
-    removeTodo: (state: AppState) => ({
+    removeTodo: (state: AppState, todo: Todo) => ({
       TODOS: {
         ...state.TODOS,
+        data: state.TODOS.data.filter(td => td.id !== todo.id),
       },
     }),
 
