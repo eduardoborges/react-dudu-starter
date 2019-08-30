7734e3241dcf97d806a227cbf010541fdb203890
Eduardo Borges <euduardoborges@gmail.com>
2019-08-30 10:49:13 -0300

    (feat): add resp and req interceptors by default

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,22 @@
+/* eslint-disable no-console */
 import axios from 'axios';
-
 
 const api = axios.create({
   baseURL: 'https://api.github.com',
+});
+
+// request interceptor
+api.interceptors.request.use((req) => {
+  // muda suas paradas
+  console.log(req);
+  return req;
+});
+
+// Response interceptor
+api.interceptors.response.use((resp) => {
+  // muda suas paradas
+  console.log(resp);
+  return resp;
 });
 
 export default api;
